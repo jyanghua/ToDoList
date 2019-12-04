6797a8ecaf952293f27b597b81a60fc7064f0600
Linda Le <lindl10@uci.edu>
2019-12-04 00:20:11 -0800

    Testing commit lol. Adding delete activity.

---
+++ app/src/main/java/com/example/todolist/DeleteItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_item);
+    }
+}
